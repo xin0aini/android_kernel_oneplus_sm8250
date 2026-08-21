30434e15c9d4d2d3275586fc2de195e545bee1f8
Chen Jinghuang <chenjinghuang2@huawei.com>
2026-08-21 17:39:49 +0530

    sched/rt: Skip currently executing CPU in rto_next_cpu()
    Fix redundant self-IPI by filtering the initiating CPU in rto_next_cpu().
    This eliminates spurious self-IPIs and prevents CPU hardlockup scenarios.
    
    Fixes: 4bdced5c9a29 ("sched/rt: Simplify the IPI based RT balancing logic")
    Suggested-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    Suggested-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Signed-off-by: Chen Jinghuang <chenjinghuang2@huawei.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1148,9 +1148,6 @@ static inline void uclamp_rq_dec(struct rq *rq, struct task_struct *p)
 	if (unlikely(!p->sched_class->uclamp_enabled))
 		return;
 
-	if (p->se.sched_delayed)
-		return;
-
 	for_each_clamp_id(clamp_id) {
 		uclamp_rq_dec_id(rq, p, clamp_id);
 		uclamp_rq_bucket_update(rq, p, clamp_id);
